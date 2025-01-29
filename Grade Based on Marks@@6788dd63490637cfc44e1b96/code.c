411bfd58398acca1201ca2f0697ee0e84914b767
CodeWithAryan1123 <aryangarg699@gmail.com>
2025-01-29 16:25:35 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -5,23 +5,23 @@ int main()
     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
     sum=a+b+c+d+e;
     percentage=(sum*100)/500;
-    if(100<=percentage>=90)
+    if(percentage>=90)
     {
         printf("A");
     }
-    else if(90<percentage>=80)
+    else if(percentage<90 && percentage>=80)
     {
         printf("B");
     }
-    else if(80<percentage>=70)
+    else if(percentage<80 && percentage>=70)
     {
         printf("C");
     }
-    else if(70<percentage<=60)
+    else if(percentage<70 && percentage>=60)
     {
         printf("D");
     }
-    else
+    else 
     {
         printf("F");
     }
